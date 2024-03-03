a9abfbb4602002c9ea3918e83026d923b2a6f470
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2024-03-03 14:22:08 -0700

    03-03-2024 Create PopularNamesData.sql
    This code INSERTS the data of the most popular boy and girl names in 2023 into the table.

---
+++ Most Popular Girl-and-Boy Names/PopularNamesData.sql
@@ -0,0 +1,11 @@
+-- This SQL code inserts the most popular girl and boy names into the table "PopularNamesTable". It contains a list of all the most popular names for girls and boys in 2023.
+-- Authored by: Kyle Shanahan
+INSERT INTO PopularNames (name, gender, rank) VALUES
+    ('Liam', 'boy', 1),
+    ('Olivia', 'girl', 1),
+    ('Noah', 'boy', 2),
+    ('Emma', 'girl', 2),
+    ('William', 'boy', 3),
+    ('Ava', 'girl', 3),
+    -- and so on...
+    ;
